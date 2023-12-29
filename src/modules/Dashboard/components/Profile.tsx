1eebc39fa5976f5267018efbd887be5d468b001c
Jenin82 <jenin8282@gmail.com>
2023-12-29 21:46:35 +0530

    dashboard styling

--- src/modules/Dashboard/components/Profile.tsx
+++ src/modules/Dashboard/components/Profile.tsx
@@ -6,8 +6,40 @@ const Profile = (_props: Props) => {
         <div className={styles.profileContainer}>
             <h1>Profile</h1>
             <div className={styles.profileContents}>
-                <div>Test</div>
-                <div>Test2</div>
+                <div className={styles.profileSection1}>
+                    <div className={styles.profilePicture}>
+                        <img
+                            src="https://avatars.githubusercontent.com/u/84625875?v=4"
+                            alt="profile picture"
+                        />
+                    </div>
+                    <div className={styles.profileInfo}>
+                        <h3>Jenin Joseph</h3>
+                        <h4>YIP intern</h4>
+                    </div>
+                </div>
+                <div className={styles.profileSection2}>
+                    <h4 className={styles.blackText}>
+                        <span className={styles.greyText}>Email: </span>
+                        jenin8282@gmail.com
+                    </h4>
+                    <h4 className={styles.blackText}>
+                        <span className={styles.greyText}>Mobile: </span>
+                        8848687988
+                    </h4>
+                    <h4 className={styles.blackText}>
+                        <span className={styles.greyText}>Gender: </span>
+                        Male
+                    </h4>
+                    <h4 className={styles.blackText}>
+                        <span className={styles.greyText}>Age: </span>
+                        23
+                    </h4>
+                    <h4 className={styles.blackText}>
+                        <span className={styles.greyText}>District: </span>
+                        Ernakulam
+                    </h4>
+                </div>
             </div>
         </div>
     );
