b789e29e37fb8e0943a74095ac40765f57dbb4a1
notS0LD13R <joelsebin@gmail.com>
2023-12-31 20:12:34 +0530

    feat(sidebar):mobile view implemented

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,51 +2,69 @@ import { MdOutlineLogout } from "react-icons/md";
 import styles from "./Sidebar.module.css";
 import { NavData } from "./services/NavData";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { BiCross, BiMenu, BiX } from "react-icons/bi";
 
 type Props = {};
 
 const Sidebar = (_props: Props) => {
     const navigate = useNavigate();
     const location = useLocation();
+    const [ismobileMenuActive, setIsMobileMenuActive] = useState(false);
 
     useEffect(() => {
         console.log(location.pathname);
     });
     return (
-        <div className={styles.sidebarContianer}>
-            <div>
-                <img
-                    src="https://yip.kerala.gov.in/wp-content/uploads/2022/10/logonew-1.png"
-                    alt="YIP logo"
-                    width={100}
-                />
-            </div>
-            <div className={styles.sidebarNav}>
-                {NavData.map((item) => (
-                    <div
-                        key={item.link}
-						onClick={() => navigate(item.link)}
-                        className={`${styles.navItem} ${
-                            item.link === location.pathname as string ? styles.active : ""
-                        }`}
-                    >
-                        <item.icon /> {item.title}
-                    </div>
-                ))}
+        <>
+            <div
+                className={
+                    styles.sidebarContianer +
+                    " " +
+                    (ismobileMenuActive ? styles.menuactive : "")
+                }
+            >
+                <div>
+                    <img
+                        src="https://yip.kerala.gov.in/wp-content/uploads/2022/10/logonew-1.png"
+                        alt="YIP logo"
+                        width={100}
+                    />
+                </div>
+                <div className={styles.sidebarNav}>
+                    {NavData.map((item) => (
+                        <div
+                            key={item.link}
+                            onClick={() => navigate(item.link)}
+                            className={`${styles.navItem} ${
+                                item.link === (location.pathname as string)
+                                    ? styles.active
+                                    : ""
+                            }`}
+                        >
+                            <item.icon /> {item.title}
+                        </div>
+                    ))}
+                </div>
+                <div
+                    className={styles.logout}
+                    onClick={() => {
+                        localStorage.removeItem("accessToken");
+                        localStorage.removeItem("refreshToken");
+                        navigate("/login");
+                    }}
+                >
+                    <MdOutlineLogout />
+                    Logout
+                </div>
             </div>
             <div
-                className={styles.logout}
-                onClick={() => {
-                    localStorage.removeItem("accessToken");
-                    localStorage.removeItem("refreshToken");
-                    navigate("/login");
-                }}
+                className={styles.hamburger}
+                onClick={() => setIsMobileMenuActive(!ismobileMenuActive)}
             >
-                <MdOutlineLogout />
-                Logout
+                {ismobileMenuActive ? <BiX size={40} /> : <BiMenu size={40} />}
             </div>
-        </div>
+        </>
     );
 };
 
